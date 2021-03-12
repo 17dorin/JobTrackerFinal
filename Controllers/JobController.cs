a08869e2eaf7045e0253701e2d3d180f338f8cdb
Jeremiah Wyeth <jeremiahwyeth@gmail.com>
2021-03-12 15:08:41 -0500

    added jobcontroller and index/search view

---
+++ Controllers/JobController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalProject.Models;
+
+namespace FinalProject.Controllers
+{
+    public class JobController : Controller
+    {
+        private JobDAL jd = new JobDAL();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Search(string country, int page)
+        {
+            string countryLower = country.Trim().ToLower();
+            Rootobject r = jd.SearchJobs(countryLower, page);
+            return View(r.results.ToList());
+        }
+    }
+}
