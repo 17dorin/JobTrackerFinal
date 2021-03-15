8a9ed729f9fc6e912318311d012a5811b786d020
Ramon <rag4@pdx.edu>
2021-03-15 09:00:23 -0700

    Edited ConfirmEmail
    ConfirmEmail has a button that redirects to a template to Skill Set Search/Checklist thingy

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Skills()
+        {
+            return View();
+        }
+    }
+}
