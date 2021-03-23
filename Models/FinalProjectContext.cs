196059a17ac1c988ff2073eccc44e11001b9e2f1
Ramon <rag4@pdx.edu>
2021-03-22 21:54:06 -0700

    hide connection
    hid sql connection that was being shown in the context model

--- Models/FinalProjectContext.cs
+++ Models/FinalProjectContext.cs
@@ -34,7 +34,7 @@ namespace FinalProject.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=gc-jobtracker.database.windows.net;Initial Catalog=FinalProject;Persist Security Info=True;User ID=user-jobtracker;Password=GrandCircu$;;Trusted_Connection=False");
+                optionsBuilder.UseSqlServer(Secret.SQLSeverConnection);
             }
         }
 
